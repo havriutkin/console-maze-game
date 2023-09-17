21297464afd576ad115b8ca4a9b87741ed4f9dda
Vladyslav Havriutkin <vlad@Jays-iMac.local>
2023-09-17 05:21:53 -0400

    Add dependencies

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('It does work!');
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field{
+    
+}
