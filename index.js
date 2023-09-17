67524d9150bfca99511fefe708fa2d897e8d69bc
Vladyslav Havriutkin <vlad@Jays-iMac.local>
2023-09-17 05:42:36 -0400

    Build Field class

--- index.js
+++ index.js
@@ -6,5 +6,65 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field{
-    
+    constructor(fieldMap){
+        this._map = fieldMap;
+        this._width = fieldMap[0].length;
+        this._height = fieldMap.length;
+        this._userPos = {x: 0, y: 0};
+    }
+
+    print(){
+        // Function prints map to console
+        console.log(this._map);
+    }
+
+    moveUser(direction){
+        // Function takes direction and changes user's position
+        switch (direction) {
+            case 'w': {
+                this._userPos.y++;
+                break;
+            }
+
+            case 'd': {
+                this._userPos.x++;
+                break
+            }
+
+            case 's': {
+                this._userPos.y--;
+                break;
+            }
+
+            case 'a': {
+                this._userPos.x--;
+                break;
+            }
+        
+            default: {
+                throw new Error('Uknown direction');
+            }
+        }
+    }
+
+    checkUser(){
+        // Function returns true if user is still alive, returns false otherwise
+        const {x, y} = this._userPos;
+
+        if (this._map[y][x] == hole) 
+            return false
+
+        if (x > this._width - 1 || x < 0)
+            return false
+
+        if (y > this._height - 1 || y < 0)
+            return false
+
+        return true;
+    }
+
+    static generateMap(width, height, percentage){
+        // Function generate random map with given percantage of holes. 
+        // TODO 
+    }
 }
